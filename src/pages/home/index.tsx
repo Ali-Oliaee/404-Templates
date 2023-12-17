69c1bd4582b1c8ba6313c0048840bb767c9d538a
ali oliaee <alioliaee31@gmail.com>
2023-12-17 18:16:07 +0330

    add svg support

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,23 @@
+import GithubLogo from "../../assets/icons/github-logo.svg?react"
+import "./styles.scss"
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="home-page">
+      <div className="header">
+        <button>
+          <GithubLogo />
+          Github
+        </button>
+      </div>
+
+      <div className="content">
+        <h1 className="title">4X4 Templates</h1>
+        <h3 className="subtitle">Free And OpenSource</h3>
+        <h6 className="description">feel free to use 😉</h6>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
