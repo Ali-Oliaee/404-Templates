1d07ef0aaa644a6ee94edd91aa85f5f0a346feee
ali oliaee <alioliaee31@gmail.com>
2023-12-17 18:28:06 +0330

    add Rubik Doodle Shadow font

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,17 +4,17 @@ import "./styles.scss"
 const HomePage: React.FC = () => {
   return (
     <div className="home-page">
-      <div className="header">
+      {/* <div className="header">
         <button>
           <GithubLogo />
           Github
         </button>
-      </div>
+      </div> */}
 
       <div className="content">
-        <h1 className="title">4X4 Templates</h1>
+        <h1 className="title">404 Templates</h1>
         <h3 className="subtitle">Free And OpenSource</h3>
-        <h6 className="description">feel free to use 😉</h6>
+        <h6 className="description">feel free to use</h6>
       </div>
     </div>
   )
