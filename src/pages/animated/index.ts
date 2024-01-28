e84d269c4dba351f5cfa1edf64bd6828f5213a80
ali oliaee <alioliaee31@gmail.com>
2024-01-28 22:47:19 +0330

    animated 4 page

--- src/pages/animated/index.ts
+++ src/pages/animated/index.ts
@@ -1,3 +1,4 @@
 export { default as Animated1 } from "./animated1"
 export { default as Animated2 } from "./animated2"
 export { default as Animated3 } from "./animated3"
+export { default as Animated4 } from "./animated4"
