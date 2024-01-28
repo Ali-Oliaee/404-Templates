e7ee8b04c5e6b6929ef0c29ba402f790003673e0
ali oliaee <alioliaee31@gmail.com>
2024-01-28 20:53:22 +0330

    animated 1 page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,3 +5,5 @@ export { Gradient1 } from "./gradient"
 export { Minimal1 } from "./minimal"
 export { Minimal2 } from "./minimal"
 export { Minimal3 } from "./minimal"
+
+export { Animated1 } from "./animated"
